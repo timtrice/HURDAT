e1f182dcdde1b1ee85d474f8685172184c406abc
timtrice <tim.trice@gmail.com>
2017-05-13 15:00:31 -0500

    Rename vignette

---
+++ inst/doc/hurdat.R
@@ -0,0 +1,6 @@
+## ----libraries, echo = FALSE, messages = FALSE---------------------------
+library(HURDAT)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  hurdat(basin = "AL")
+
