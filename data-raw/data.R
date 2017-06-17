1023d73d25efe344a160ecbdcc01f40e124fdceb
timtrice <tim.trice@gmail.com>
2017-06-17 09:31:54 -0500

    Add csv data; ignore in Rbuild

--- data-raw/data.R
+++ data-raw/data.R
@@ -3,7 +3,9 @@ library(HURDAT)
 ## ---- Get Atlantic -----------------------------------------------------------
 AL <- get_hurdat(basin = "AL")
 devtools::use_data(AL, pkg = ".", overwrite = TRUE)
+readr::write_csv(AL, path = "./data/AL.csv")
 
 ## -- Get Pacific --------------------------------------------------------------
 EP <- get_hurdat(basin = "EP")
 devtools::use_data(EP, pkg = ".", overwrite = TRUE)
+readr::write_csv(EP, path = "./data/EP.csv")
