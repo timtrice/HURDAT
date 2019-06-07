3d1a3a0919d52f7eea795dec30321e1ea1cb32d5
Tim Trice <tim.trice@gmail.com>
2019-06-07 08:36:21 -0500

    Change package for func `use_data`

--- data-raw/data.R
+++ data-raw/data.R
@@ -2,10 +2,10 @@ library(HURDAT)
 
 ## ---- Get Atlantic -----------------------------------------------------------
 AL <- get_hurdat(basin = "AL")
-devtools::use_data(AL, pkg = ".", overwrite = TRUE)
+usethis::use_data(AL, overwrite = TRUE)
 readr::write_csv(AL, path = "./data/AL.csv")
 
 ## -- Get Pacific --------------------------------------------------------------
 EP <- get_hurdat(basin = "EP")
-devtools::use_data(EP, pkg = ".", overwrite = TRUE)
+usethis::use_data(EP, overwrite = TRUE)
 readr::write_csv(EP, path = "./data/EP.csv")
