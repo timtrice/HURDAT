247d44c1ca2bf31c7496996b98db6a8b253b908f
Tim Trice <tim.trice@gmail.com>
2019-07-13 06:48:32 -0500

    Do not write CSVs

--- data-raw/data.R
+++ data-raw/data.R
@@ -3,9 +3,7 @@ library(HURDAT)
 ## ---- Get Atlantic -----------------------------------------------------------
 AL <- get_hurdat(basin = "AL")
 usethis::use_data(AL, overwrite = TRUE)
-readr::write_csv(AL, path = "./data/AL.csv")
 
 ## -- Get Pacific --------------------------------------------------------------
 EP <- get_hurdat(basin = "EP")
 usethis::use_data(EP, overwrite = TRUE)
-readr::write_csv(EP, path = "./data/EP.csv")
