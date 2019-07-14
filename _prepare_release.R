8b2ad86474237fddbc6a87e6eeba3363e17f1b0d
Tim Trice <tim.trice@gmail.com>
2019-07-14 09:40:48 -0500

    Create pre-CRAN-submittal before release

---
+++ _prepare_release.R
@@ -0,0 +1,8 @@
+#' Run checks before submitting to CRAN
+devtools::document()
+devtools::spell_check()
+devtools::check_rhub()
+devtools::check_win_devel()
+devtools::check_win_release()
+devtools::check_win_oldrelease()
+devtoos::release()
